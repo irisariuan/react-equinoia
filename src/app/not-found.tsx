8c5df5aa563775886bb03596f780a3dd465bac71
CoolAlvin769 <kms12836@gmail.com>
2023-08-31 16:15:12 +0800

    Not found page added icon in it

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,8 +5,9 @@ export default function() {
     return (
         <div className="h-screen flex flex-col">
             <Navbar />
-            <div className="flex flex-1 justify-center content-center items-center justify-center">
-                <div className="text-center">
+            <div className="flex flex-1 justify-center content-center items-center">
+                <div className="text-center flex items-center justify-center flex-col">
+                <img src="icon.jpg" alt="Icon of Equinoia" className="w-1/4 z-0 rounded-full" />
                     <p className="text-3xl ">Opps! Page Not Found!</p>
                     <p>You may want to go to our <Link href="/" className="text-blue-600 hover:text-blue-800">home page</Link></p>
                 </div>
