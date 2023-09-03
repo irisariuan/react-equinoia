fd57cfa850b3e7f2aa14ca51f0dc9b516503cfdf
CoolAlvin769 <kms12836@gmail.com>
2023-09-03 18:45:35 +0800

    navlist update
    removed election platform and replaced it with welfare only

--- src/lib/nav/navList.tsx
+++ src/lib/nav/navList.tsx
@@ -4,13 +4,8 @@ export const navList: LinkObject[] = [
     { title: "Home", content: "/" },
     { title: "About Us", content: "/about" },
     {
-        title: "Election Platform",
-        content: [
-            { title: "Internal Activities", content: "/election/internal" },
-            { title: "External Activites", content: "/election/external" },
-            { title: "Welfare", content: "/election/welfare" },
-            { title: "Policies", content: "/election/policy" },
-        ],
+        title: "Welfare",
+        content: "/welfare"
     },
     { title: "Supporting Schools", content: "/supportSchools" },
     { title: "Photos", content: "/photo" },
