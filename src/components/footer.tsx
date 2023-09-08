d497008c3d9ce36063e44ddd60f3ba33ca688c8a
irisariuan <kms12836@gmail.com>
2023-09-08 18:05:30 +0800

    first build test

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,4 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,7 +11,7 @@ export default function () {
                 <p className="text-xl font-bold mb-5">Contact Us</p>
                 <div className="flex flex-row items-center justify-center gap-2">
                     <Link href="https://www.instagram.com/" target="_blank" className="flex items-center justify-center [&>]:hover:text-blue-600" >
-                        <FontAwesomeIcon icon={faInstagram} className="mr-2 text-xl" />
+                        <FontAwesomeIcon icon={faInstagram as IconProp} className="mr-2 text-xl" />
                         <span className="text-sm lg:text-lg bg-clip-text from-orange-600 to-purple-600 bg-gradient-to-r hover:text-transparent">Instagram</span>
                     </Link>
                     <div className="h-4 w-[1px] bg-rice-content" />
--- src/components/schoolListItem.tsx
+++ src/components/schoolListItem.tsx
@@ -7,6 +7,7 @@ import { Label } from "@/components/ui/label"
 import { School } from "@/lib/csv/readCsv"
 import { ListItem } from "@/components/ui/list"
 import { useSize } from "@/lib/useSize"
+import { IconProp } from "@fortawesome/fontawesome-svg-core"
 
 export function SchoolListItem({school} : {school: School}) {
     const isMobile = useSize()
@@ -42,7 +43,7 @@ export function DesktopListItem({ school }: { school: School }) {
                     <div className="flex items-center justify-center">
                         <div className="mx-2 w-px h-4 bg-rice-content inline-block" />
                         <Link href={'https://www.instagram.com/' + school.instagramName} className="flex items-center">
-                            <FontAwesomeIcon icon={faInstagram} className="mr-1 text-orange-600" />
+                            <FontAwesomeIcon icon={faInstagram as IconProp} className="mr-1 text-orange-600" />
                             <span className="bg-clip-text from-orange-600 to-purple-600 bg-gradient-to-r text-transparent text-lg lg:text-xl">{school.instagramName}</span>
                         </Link>
                     </div>
