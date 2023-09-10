d4cb7f8e16532ae8482afe54d649baf0346134aa
Ariuan <39485107+irisariuan@users.noreply.github.com>
2023-09-10 17:00:32 +0000

    changes

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -7,11 +7,14 @@ export default function () {
             <div className='flex flex-col justify-center items-center'>
                 <div className="flex items-center justify-center mb-6">
                     <img src="school_icon.png" alt="Methodist College" className='z-0 h-12 w-12 md:h-16 md:w-16 lg:h-24 lg:w-24 mr-2' />
-                    <h1 className={"text-7xl md:text-9xl text-center text-gold-400/70 mr-2 "}>Equinoia</h1>
+                    <div className="">
+                        <h1 className={"text-7xl md:text-9xl text-center text-gold-400/70 mr-2 mb-4 "}>Equinoia</h1>
+                        <span className="text-gold-300 self-start">Proposed Students' Union Candidate No.1</span>
+                    </div>
                     <span className='[writing-mode:vertical-lr] text-gold-200 text-xl mt-4 sm:text-lg'>2023-2024</span>
                 </div>
                 <span className={' mt-4 text-gold-300 text-lg md:text-3xl'}>Spread your wings and fly</span>
-                <span className={' text-gold-300 text-lg md:text-3xl'}>Equinoia raises you high</span>
+                <span className={' text-gold-300 text-lg md:text-3xl'}>Equinoia raise you high</span>
             </div>
         </motion.div>
     )
