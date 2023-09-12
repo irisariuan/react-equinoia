21906b685392230290e3a71ff10fa01505f088e0
CoolAlvin769 <kms12836@gmail.com>
2023-09-12 23:22:51 +0800

    minor fixes

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -9,7 +9,7 @@ export default function () {
                     <img src="school_icon.png" alt="Methodist College" className='z-0 h-12 w-12 md:h-16 md:w-16 lg:h-24 lg:w-24 mr-2' />
                     <div className="">
                         <h1 className={"text-7xl md:text-9xl text-center text-gold-400/70 mr-2 mb-4 "}>Equinoia</h1>
-                        <span className="text-gold-300 self-start">Proposed Students' Union Candidate No.1</span>
+                        <span className="text-gold-300 self-start">Proposed Students' Union Candidate Cabinet No.1</span>
                     </div>
                     <span className='[writing-mode:vertical-lr] text-gold-200 text-xl mt-4 sm:text-lg'>2023-2024</span>
                 </div>
