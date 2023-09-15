bc74f8b5b054291443daf2d7741276906a73da2c
CoolAlvin769 <39485107+CoolAlvin769@users.noreply.github.com>
2023-09-16 00:18:07 +0800

    change fixedInstagramIcon size

--- src/components/fixedInstagramIcon.tsx
+++ src/components/fixedInstagramIcon.tsx
@@ -4,8 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function () {
     return (
-        <a href='https://www.instagram.com/equinoia.mc/' className="bg-rice-content/60 border border-rice-dark/20 fixed bottom-0 right-0 backdrop-blur-xl rounded-full z-40 p-4 w-12 h-12 lg:w-20 lg:h-20 m-6 lg:m-8 flex justify-center items-center">
-            <FontAwesomeIcon className="text-white text-xl lg:text-4xl" icon={faInstagram as IconProp} />
+        <a href='https://www.instagram.com/equinoia.mc/' className="bg-rice-content/60 border border-rice-dark/20 fixed bottom-0 right-0 backdrop-blur-xl rounded-full z-40 p-4 w-16 h-16 lg:w-20 lg:h-20 m-6 lg:m-8 flex justify-center items-center">
+            <FontAwesomeIcon className="text-white text-3xl lg:text-4xl" icon={faInstagram as IconProp} />
         </a>
     )
 }
