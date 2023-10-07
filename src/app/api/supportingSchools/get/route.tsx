d51f4b0283b58de8ee4231b1e074d9f813184b02
irisariuan <kms12836@gmail.com>
2023-10-07 22:29:22 +0800

    force api to be dynamic

--- src/app/api/supportingSchools/get/route.tsx
+++ src/app/api/supportingSchools/get/route.tsx
@@ -1,6 +1,9 @@
 import { readSchools } from "@/lib/fs/school"
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export async function GET(request: Request) {
     const schools = await readSchools()
-    return Response.json({schools})
+    return Response.json({ schools })
 }
