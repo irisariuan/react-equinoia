76ba6cf88d01a286e7839e4b7316182f310010c5
irisariuan <kms12836@gmail.com>
2023-10-07 21:11:32 +0800

    redis storage update hotfix

--- src/app/api/supportingSchools/get/route.tsx
+++ src/app/api/supportingSchools/get/route.tsx
@@ -1,6 +1,6 @@
 import { readSchools } from "@/lib/fs/school"
 
 export async function GET(request: Request) {
-    const school = readSchools(process.cwd() + '/src/private/newSupportingSchoolList.csv')
+    const school = readSchools()
     return Response.json({school})
 }
