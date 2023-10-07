9584a37495323b4872b55a3e966a2835d77bc926
irisariuan <kms12836@gmail.com>
2023-10-07 23:42:58 +0800

    cors options added in edit api

--- src/app/api/supportingSchools/edit/route.tsx
+++ src/app/api/supportingSchools/edit/route.tsx
@@ -5,6 +5,19 @@ import { readToken } from "@/lib/fs/token"
 export const dynamic = 'auto'
 export const revalidate = 0
 
+export async function OPTIONS(request: Request) {
+    return new Response(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+        "Access-Control-Max-Age": "86400",
+      },
+    })
+  }
+
 export async function POST(request: Request) {
     if (request.method !== 'POST') {
         return Response.error()
