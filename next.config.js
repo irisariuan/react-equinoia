8bc62821a08c2648ec295f0e3e4806298fcf7180
irisariuan <kms12836@gmail.com>
2023-10-07 23:53:25 +0800

    cors final attempt

--- next.config.js
+++ next.config.js
@@ -1,28 +1,15 @@
 /** @type {import('next').NextConfig} */
 // next.config.js
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
+    async rewrites() {
+        return [
             {
-              key: "Access-Control-Allow-Methods",
-              value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                source: '/api/:path*',
+                destination: 'https://api.example.com/:path*',
             },
-            {
-              key: "Access-Control-Allow-Headers",
-              value:
-                "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-            },
-          ],
-        },
-      ]
+        ]
     },
-  }
-  
+}
+
 
 module.exports = nextConfig
