31b60d2f38a2df47d93d6d70ee8318d1eb126a8a
irisariuan <kms12836@gmail.com>
2023-10-07 17:40:52 +0800

    api bug fix

--- src/app/api/supportingSchools/edit/route.tsx
+++ src/app/api/supportingSchools/edit/route.tsx
@@ -2,17 +2,17 @@ import { SchoolEditRequest, readCsv, writeCsv } from "@/lib/fs/csv"
 import { createHash } from 'node:crypto'
 import { readToken } from "@/lib/fs/token"
 
-export const dynamic = 'true'
+export const dynamic = 'auto'
 
 export async function POST(request: Request) {
     if (request.method !== 'POST') {
         return Response.error()
     }
 
-    const setting = readToken() 
+    const setting = readToken()
     const r: SchoolEditRequest = await request.json()
     if (!r) {
-        return Response.json({message: 'Request JSON error'}, {status: 400})
+        return Response.json({ message: 'Request JSON error' }, { status: 400 })
     }
 
     if (!setting.supportingSchools.edit.includes(createHash('sha256').update(r.token).digest('hex'))) {
@@ -21,31 +21,31 @@ export async function POST(request: Request) {
 
     if (r.type === 'change') {
         writeCsv(process.cwd() + '/src/private/newSupportingSchoolList.csv', r.data)
-        return Response.json({message: 'OK'}, {status: 200})
+        return Response.json({ message: 'OK' }, { status: 200 })
     }
-    
+
     let f = readCsv(process.cwd() + '/src/private/newSupportingSchoolList.csv')
     if (!f) {
-        return Response.json({message: 'Internal Server Error'}, {status: 500})
+        return Response.json({ message: 'Internal Server Error' }, { status: 500 })
     }
-    
+
     for (const school of r.data) {
         switch (r.type) {
             case 'add':
                 if (f.find(s => s.schoolName.chineseName.trim() === school.schoolName.chineseName.trim())) {
-                    return Response.json({message: 'School already exists'}, {status: 400})
+                    return Response.json({ message: 'School already exists' }, { status: 400 })
                 }
                 f.push(school)
                 break
             case 'remove':
                 const i = f.findIndex(s => s.schoolName.chineseName.trim() === school.schoolName.chineseName.trim())
                 if (i === -1) {
-                    return Response.json({message: 'School does not exist'}, {status: 400})
+                    return Response.json({ message: 'School does not exist' }, { status: 400 })
                 }
                 f.splice(i, 1)
                 break
         }
     }
     writeCsv(process.cwd() + '/src/private/newSupportingSchoolList.csv', f)
-    return Response.json({message: 'OK'}, {status: 200})
+    return Response.json({ message: 'OK' }, { status: 200 })
 }
