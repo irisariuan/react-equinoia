6492f49d6dd7969df4c4db692fb50b232f5a1485
irisariuan <kms12836@gmail.com>
2024-02-01 00:15:27 +0800

    add feedback page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,114 +3,76 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com',
-=======
             url: 'https://www.equinoia.online',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/about',
-=======
             url: 'https://www.equinoia.online/about',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.8,
+            priority: 0.9,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/election',
-=======
             url: 'https://www.equinoia.online/election',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
-            changeFrequency: 'monthly',
+            changeFrequency: 'yearly',
             priority: 0.5,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/photo',
-=======
             url: 'https://www.equinoia.online/photo',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.5,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/supportSchools',
-=======
             url: 'https://www.equinoia.online/supportSchools',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.7,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/welfare',
-=======
             url: 'https://www.equinoia.online/welfare',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.9,
+            priority: 0.8,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/welfare/accessories',
-=======
             url: 'https://www.equinoia.online/welfare/accessories',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.9,
+            priority: 0.5,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/welfare/catering',
-=======
             url: 'https://www.equinoia.online/welfare/catering',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.9,
+            priority: 0.5,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/welfare/clothing',
-=======
             url: 'https://www.equinoia.online/welfare/clothing',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.9,
+            priority: 0.5,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/welfare/entertainment',
-=======
             url: 'https://www.equinoia.online/welfare/entertainment',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.9,
+            priority: 0.5,
         },
         {
-<<<<<<< HEAD
-            url: 'https://www.equinoia.com/welfare/others',
-=======
             url: 'https://www.equinoia.online/welfare/others',
->>>>>>> aeb4a4ffad80dd84e91ec9122ab779ed5012475f
             lastModified: new Date(),
             changeFrequency: 'monthly',
-            priority: 0.9,
+            priority: 0.5,
+        },
+        {
+            url: 'https://www.equinoia.online/feedback',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.7,
         },
     ]
 }
